5bda4a6f20524f7155344715792fcdcb495b4c86
hjkang306 <109262091+fkdldhs8484@users.noreply.github.com>
2022-10-24 12:31:02 +0900

    10/24

--- BODA/php/connect/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "fkdldhs8484";
-    $pass = "dnehdWld159!";
-    $db = "fkdldhs8484";
-    $connect = new mysqli($host, $user, $pass, $db);
-    $connect -> set_charset("utf-8");
-?>
