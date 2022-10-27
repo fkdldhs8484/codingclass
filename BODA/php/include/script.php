77eff065baec51f79aa3159028f7358e65b5b773
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-10-27 10:19:32 +0900

    2022.10.27

--- BODA/php/include/script.php
+++
@@ -1,7 +0,0 @@
-<!-- script -->
-<script src="../../html/assets/js/headermenu.js"></script>
-<script src="../../html/assets/js/loginpopup.js"></script>
-<script src="../../html/assets/js/review.js"></script>
-<script src="../../html/assets/js/swiper.js"></script>
-<script src="../../html/assets/js/agree.js"></script>
-<!-- //script -->
